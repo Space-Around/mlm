6528fc2096958b38fcbbbb425efc155416e338f1
Space Around <viksnamax@mail.ru>
2021-04-22 03:40:42 +0300

    Delete trash files and create .gitignore

--- samples/test.py
+++
@@ -1,6 +0,0 @@
-import time
-from create_payouts import CreatePayouts
-from get_payouts import GetPayouts
-from get_payout_item import GetPayoutItem
-
-create_response = CreatePayouts().create_payouts("sb-zs0j85931255@personal.example.com", "65", False)
