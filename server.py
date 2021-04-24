3f5a51b5b2332fde1f824c25d8da9a95d356782e
Space Around <viksnamax@mail.ru>
2021-04-24 15:09:47 +0300

    +

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ import json
 from dateutil.parser import parse
 import db
 import time
+from paypalhttp import HttpError
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -83,90 +84,107 @@ def activate(key, token):
         
 
 def upgrade(lvl, key, token): 
-    response = paypal.AuthorizeOrder().authorize_order(token, debug=False)
-
-    if response.status_code == 201:
-        authorization_id = response.result.purchase_units[0].payments.authorizations[0].id
-        message_text_user_info = ""
-        message_text_seller = ""
-        amount = ""
-
-        user_info = db.get_user_info(key)
-        seller = db.get_user_info_by_id(user_info['seller_' + str(lvl) + '_id'])
-
-        seller_paypal = seller['paypal']
+    try:
+        response = paypal.AuthorizeOrder().authorize_order(token, debug=False)
         
-        if lvl == 2:
-            amount = config.LVL_2_AMOUNT
-            message_text_user_info = "Оплата прошла успешна. Активирован 2ой уровень."
-        
-        if lvl == 3:
-            amount = config.LVL_3_AMOUNT
-            message_text_user_info = "Оплата прошла успешна. Активирован 3ий уровень."
+        if response.status_code == 201:
 
-        if lvl == 4:
-            amount = config.LVL_4_AMOUNT
-            message_text_user_info = "Оплата прошла успешна. Активирован 4ый уровень."
+            print(response.result.status)
+            
+
+            authorization_id = response.result.purchase_units[0].payments.authorizations[0].id
+            message_text_user_info = ""
+            message_text_seller = ""
+            amount = ""
+
+            user_info = db.get_user_info(key)
+            seller = db.get_user_info_by_id(user_info['seller_' + str(lvl) + '_id'])
+
+            seller_paypal = seller['paypal']
+            
+            if lvl == 2:
+                amount = config.LVL_2_AMOUNT
+                message_text_user_info = "Оплата прошла успешна. Активирован 2ой уровень."
+            
+            if lvl == 3:
+                amount = config.LVL_3_AMOUNT
+                message_text_user_info = "Оплата прошла успешна. Активирован 3ий уровень."
+
+            if lvl == 4:
+                amount = config.LVL_4_AMOUNT
+                message_text_user_info = "Оплата прошла успешна. Активирован 4ый уровень."
 
 
-        if seller['lvl'] <= user_info['lvl']:
-            seller_paypal = config.ADMINE_PAYPAL
-        else:
-            if (seller['lvl'] == 2 and seller['key_gen'] == 8) or (seller['lvl'] == 3 and seller['key_gen'] == 33):
+            if seller['lvl'] <= user_info['lvl']:
                 seller_paypal = config.ADMINE_PAYPAL
-                bot.send_message(seller['tg_chat_id'], "Пользователь хотел преобрести у Вас ключ, но у Вы достигли лимита генераций ключей, повысте уровень при помощи команды /upgrade")        
             else:
-                if user_info['tg_user_name'] is None:
-                    message_text_seller = "Пользователь " + user_info['tg_user_id'] + " приобрёл ключ " + str(lvl) + " уровня за " + amount + "$"
-                else:
-                    message_text_seller = "Пользователь " + user_info['tg_user_name'] + " приобрёл ключ " + str(lvl) + " уровня за " + amount + "$"
-
-                bot.send_message(seller['tg_chat_id'], message_text_seller)
-                
-                seller['key_gen'] = seller['key_gen'] + 1
-
-                db.update_user_info(seller)
-
                 if (seller['lvl'] == 2 and seller['key_gen'] == 8) or (seller['lvl'] == 3 and seller['key_gen'] == 33):
-                    bot.send_message(seller['tg_chat_id'], "Вы достигли лимита генераций ключей на текухем уровне, повысте уровень при помощи команды /upgrade")        
+                    seller_paypal = config.ADMINE_PAYPAL
+                    bot.send_message(seller['tg_chat_id'], "Пользователь хотел преобрести у Вас ключ, но у Вы достигли лимита генераций ключей, повысте уровень при помощи команды /upgrade")        
+                else:
+                    if user_info['tg_user_name'] is None:
+                        message_text_seller = "Пользователь " + user_info['tg_user_id'] + " приобрёл ключ " + str(lvl) + " уровня за " + amount + "$"
+                    else:
+                        message_text_seller = "Пользователь " + user_info['tg_user_name'] + " приобрёл ключ " + str(lvl) + " уровня за " + amount + "$"
 
-        create_response = paypal.CreatePayouts().create_payouts(seller_paypal, amount, False)
+                    bot.send_message(seller['tg_chat_id'], message_text_seller)
+                    
+                    seller['key_gen'] = seller['key_gen'] + 1
 
-        if create_response.status_code == 201:
-            batch_id = create_response.result.batch_header.payout_batch_id
-            print('Retrieving Payouts batch with id: ' + batch_id)
-            get_response = paypal.GetPayouts().get_payouts(batch_id, False)
-            if get_response.status_code == 200:
-                item_id = get_response.result.items[0].payout_item_id
-                print('Retrieving Payout item with id: ' + item_id)
-                get_item_response = paypal.GetPayoutItem().get_payout_item(item_id, False)
-                if get_item_response.status_code == 200:
-                    print('Check Payouts status to see if it has completed processing all payments')
-                    for i in range(5):
-                        time.sleep(2)
-                        get_response = paypal.GetPayouts().get_payouts(batch_id, False)
-                        if get_response.result.batch_header.batch_status == "SUCCESS":
-                            print("Payout success")          
+                    db.update_user_info(seller)
 
-                            user_info['lvl_' + str(lvl) + '_payed'] = 1
+                    if (seller['lvl'] == 2 and seller['key_gen'] == 8) or (seller['lvl'] == 3 and seller['key_gen'] == 33):
+                        bot.send_message(seller['tg_chat_id'], "Вы достигли лимита генераций ключей на текухем уровне, повысте уровень при помощи команды /upgrade")        
+
+            create_response = paypal.CreatePayouts().create_payouts(seller_paypal, amount, False)
+
+            if create_response.status_code == 201:
+                batch_id = create_response.result.batch_header.payout_batch_id
+                print('Retrieving Payouts batch with id: ' + batch_id)
+                get_response = paypal.GetPayouts().get_payouts(batch_id, False)
+                if get_response.status_code == 200:
+                    item_id = get_response.result.items[0].payout_item_id
+                    print('Retrieving Payout item with id: ' + item_id)
+                    get_item_response = paypal.GetPayoutItem().get_payout_item(item_id, False)
+                    if get_item_response.status_code == 200:
+                        print('Check Payouts status to see if it has completed processing all payments')
+                        for i in range(5):
+                            time.sleep(2)
+                            get_response = paypal.GetPayouts().get_payouts(batch_id, False)
+                            if get_response.result.batch_header.batch_status == "SUCCESS":
+                                print("Payout success")          
+
+                                user_info['lvl_' + str(lvl) + '_payed'] = 1
 
 
-                            user_info['lvl'] = lvl
-                            user_info['key_gen'] = 0
+                                user_info['lvl'] = lvl
+                                user_info['key_gen'] = 0
 
-                            db.update_user_info(user_info)
+                                db.update_user_info(user_info)
 
-                            bot.send_message(user_info['tg_chat_id'], message_text_user_info)
-                            break
+                                bot.send_message(user_info['tg_chat_id'], message_text_user_info)
+                                break
 
-                    if i == 4:
-                        print('Payouts batch is not processed yet')
+                        if i == 4:
+                            print('Payouts batch is not processed yet')
+                else:
+                    print('Failed to retrieve Payouts batch with id: ' + batch_id)
             else:
-                print('Failed to retrieve Payouts batch with id: ' + batch_id)
+                print('Failed to create Payouts batch')        
         else:
-            print('Failed to create Payouts batch')        
-    else:
-        print("Link is unreachable")        
+            print("Link is unreachable")        
+
+    except IOError as ioe:
+        if isinstance(ioe, HttpError):
+            error_json = json.loads(ioe.message)
+            issue = error_json['details'][0]['issue']
+            description = error_json['details'][0]['description']
+            text = ""
+
+            if issue == "MAX_NUMBER_OF_PAYMENT_ATTEMPTS_EXCEEDED":
+                text = "Paypal issue:\n" + "issue: " + issue + "\ndescription: " + description
+        
+            return text
         
 
 async def handle(request):
@@ -178,19 +196,20 @@ async def handle(request):
     key = decrypted_data.split()[0]
     action = decrypted_data.split()[1]
 
+    text = "Дождитесь редиректа в telegram"
+
     if action == ACTIONE_ACTIVATE:
         activate(key, token)
 
     if action == ACTIONE_UPGRADE_LVL_2:
-        upgrade(2, key, token)
+         = upgrade(2, key, token)
 
     if action == ACTIONE_UPGRADE_LVL_3:
-        upgrade(3, key, token)
+        text = upgrade(3, key, token)
 
     if action == ACTIONE_UPGRADE_LVL_4:
         upgrade(4, key, token)
-
-    text = "Дождитесь редиректа в telegram"
+    
     return web.Response(text=text)
 
 
